d479a7391cd38af6fd1cffcd7461cc5bd3b9ca2e
evaldobarbosa <evaldobarbosa@gmail.com>
2015-08-03 16:42:09 -0300

    tba provider

--- src/TBA/Boilerplate/Silex/Skeleton/Service/TBAProvider.php
+++ src/TBA/Boilerplate/Silex/Skeleton/Service/TBAProvider.php
@@ -10,13 +10,13 @@ class TBAProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
     	$app['tba'] = $app->share(function() use ($app) {
-    		$config = array(
-    			'table_name' => $app['tba.table_name'],
-    			'user_field' => $app['tba.user_field'],
-    			'pass_field' => $app['tba.pass_field'],
-    			'token_timeout' => $app['tba.token_timeout'],
-                'salt' => $app['tba.salt']
-    		);
+    		$config = array();
+            $config['table_name'] = $app['tba.table_name'];
+    		$config['user_field'] = $app['tba.user_field'];
+    		$config['pass_field'] = $app['tba.pass_field'];
+    		$config['token_timeout'] = $app['tba.token_timeout'];
+            $config['salt'] = $app['tba.salt'];
+
     		$tba = new TokenBasedAuth( $config );
 
     		return $tba;
